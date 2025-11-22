786840c1f59faf2e8b9479e4dc87fc7151ce5f1a
rohmanalka <rohmanalka06@gmail.com>
2025-11-22 19:42:37 +0700

    chore(firebase): add firebase packages and initialize in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'pages/login_page.dart';
 import 'pages/home_page.dart';
 import 'pages/riwayat_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
